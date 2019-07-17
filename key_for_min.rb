78dd561b80eab7c953533ed883ba98c606e40544
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-17 17:00:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,12 @@ def key_for_min_value(name_hash)
   smallest_key = nil 
   
   name_hash.each do |item, amount|
-    if amount < smallest_value
-      smallest_value += value
-      
+        if smallest_value == 0 || amount < smallest_value
+      smallest_value = amount
+      smallest_key = item
+    end
+  end
+  smallest_key
+end
 end
 

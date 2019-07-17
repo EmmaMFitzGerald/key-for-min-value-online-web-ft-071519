4c2cf7eed022da8a19a88f165aeeb23975948d11
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-17 16:44:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,3 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+
+
+def num_points_scored(players_name)
+  hash = game_hash
+  hash.each do |place, team|
+    team.each do |characteristic, info|
+      if characteristic == :players
+        info.each do |player, stats|
+          if player == players_name
+              return stats[:points]
+          end
+        end
+      end
+    end
+  end
+end
